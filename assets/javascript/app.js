ba3204b950af34183db63ce1be89da2d4e38e87b
Fernando Marinho <fernando.marinho@uconn.edu>
2019-09-09 07:32:45 -0400

    JS in development

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,45 @@ console.log("Hello, this is Ferd's Code")
 
 //VARIABLES
 
-const triviaDisplay = document.getElementById('trivia');
-const submitDisplay = document.getElementById('submit');
-const resultsDisplay = document.getElementById('results');
+var triviaDisplay = document.getElementById('trivia');
+var submitDisplay = document.getElementById('submit');
+var resultsDisplay = document.getElementById('results');
 
-function showTrivia(){}
+function showTrivia(){
 
+     var output = [];
+
+  // This 
+  theseQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // we'll want to store the list of answer choices
+      var answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+       // add this question and its answers to the output
+       output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+        // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+//remove later showTrivia = buildQuiz
 function showResults(){}
 
 //HERE is the Start button, that will eventually show the trivia 
@@ -21,7 +54,7 @@ submitButton.addEventListener('click', showResults);
 
 //Building the trivia
 
-const myQuestions = [
+var theseQuestions = [
     {
       question: "Who is the strongest?",
       answers: {
@@ -29,7 +62,7 @@ const myQuestions = [
         b: "The Terminator",
         c: "Waluigi, obviously"
       },
-      correctAnswer: "c"
+      rightAnswer: "c"
     },
     {
       question: "What is the best site ever created?",
@@ -38,7 +71,7 @@ const myQuestions = [
         b: "Simple Steps Code",
         c: "Trick question; they're both the best"
       },
-      correctAnswer: "c"
+      rightAnswer: "c"
     },
     {
       question: "Where is Waldo really?",
@@ -46,8 +79,78 @@ const myQuestions = [
         a: "Antarctica",
         b: "Exploring the Pacific Ocean",
         c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
       },
-      correctAnswer: "d"
+      rightAnswer: "c"
     }
   ];
+//PART 3 --------------------------------------------------------------------------------
+
+function showTrivia(){
+
+var output = [];
+
+  // This 
+  theseQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // we'll want to store the list of answer choices
+      var answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+       // add this question and its answers to the output
+       output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+        // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+}
+PART 4 --------------------------------------------------
+
+function showResults(){
+
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+    // keep track of user's answers
+    let numCorrect = 0;
+  
+    // for each question...
+    theseQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+      // find selected answer
+      var answerContainer = answerContainers[questionNumber];
+      var selector = 'input[name=question'+questionNumber+']:checked';
+      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+      // if answer is correct
+      if(userAnswer===currentQuestion.correctAnswer){
+        // add to the number of correct answers
+        numCorrect++;
+  
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+  
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + ' out of ' + theseQuestions.length;
+  }
+
