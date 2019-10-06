bb70e605fe71cc7cd6c658e072897a23f8b245d6
Fernando Marinho <fernando.marinho@uconn.edu>
2019-10-06 16:25:25 -0400

    revisions for homework

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# TriviaGame
-Homework 5 JQuery
+# TriviaGameBasic
+
+Basic Trivia Game using HTML, CSS, JavaScript, and jQuery
+
+Link to Deployed Project: https://jrol.github.io/TriviaGameBasic/
+
+Problem: Must create a Trivia game using JavaScript for the logic and jQuery to manipulate HTML. Be sure to layout this app with valid HTML and stylish CSS.
+
+Solution: Using jQuery to manipulate HTML and update whether the Start button, Trivia questions, or Trivia score, are currently displaying. 
+
+Technical Approach: Utilize jQuery to manipulate and update HTML.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,102 +1,291 @@
-(function() {
-  function showTrivia() {
-    // we'll need a place to store the HTML output
-    var output = [];
+var number = 30;
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      var answers = [];
+var intervalId;
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
+// Questions array
+var questions = [{
+    question: "Which album sold more records?",
+    answerList: ["Alanis Morissette's \"Jagged Little Pill\"", "Spice Girls' \"Spice\"", "Shania Twain's \"Come On Over\"", "Metallica's \"Metallica\""],
+    answer: 0
+},{
+    question: "\"Tamagotchi\" is a Japanese portmanteau of what two words?",
+    answerList: ["\"keychain\" and \"creature\"", "\"pocket\" and \"animal\"", "\"egg\" and \"watch\"", "\"handheld\" and \"clock\""],
+    answer: 2
+},{
+    question: "What was the flavor of the soft drink Surge?",
+    answerList: ["Lemon", "Lime", "Tropical", "Citrus"],
+    answer: 3
+},{
+    question: "Who sang the \"Friends\" theme song?",
+    answerList: ["Spin Doctors", "The Wallflowers", "Gin Blossoms", "The Rembrandts"],
+    answer: 3
+},{
+    question: "The 1996 Summer Olympics where held in which U.S. city?",
+    answerList: ["San Antonio", "Atlanta", "Denver", "Houston"],
+    answer: 1
+},{
+    question: "What is the name of the high school in \"Boy Meets World\"?",
+    answerList: ["John Adams High School", "John Henry High School", "John Hancock High School", "Johnny Appleseed High School"],
+    answer: 0
+},{
+    question: "When the iBook was released in 1999, which was NOT one of the five colors available?",
+    answerList: ["Tangerine", "Blueberry", "Key Lime", "Watermelon"],
+    answer: 3
+}];
+
+$("#start").on("click", function() {
+
+    // Hide Start button
+    $(this).hide();
+    $("#brain-power").hide();
+
+    // Display initial time countdown
+    $("#time").html("<h2>Time Remaining: 30 Seconds</h2>" + "<br>");
+    $("#clock-ticks").html("<p>The clock is ticking!</p>" + "<br>");
+
+
+    // Start timer countdown
+    run();
+   
+    // Display questions --- I still want to turn this into a reusable piece so that i don't have to repeat this section for each question
+    // Question 1
+    $("#question1").html("<h3>" + questions[0].question + "</h3>");
+    $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
+        + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answerList[1] + "</label>"
+        + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answerList[2] + "</label>"
+        + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 2
+    $("#question2").html("<h3>" + questions[1].question + "</h3>");
+    $("#answer2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[1].answerList[0] + "</label>"
+        + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[1].answerList[1] + "</label>"
+        + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[1].answerList[2] + "</label>"
+        + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[1].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 3
+    $("#question3").html("<h3>" + questions[2].question + "</h3>");
+    $("#answer3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[2].answerList[0] + "</label>"
+        + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[2].answerList[1] + "</label>"
+        + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[2].answerList[2] + "</label>"
+        + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[2].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 4
+    $("#question4").html("<h3>" + questions[3].question + "</h3>");
+    $("#answer4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[3].answerList[0] + "</label>"
+        + "<input type='radio' name='answer4' value='1'>" + "<label>" + questions[3].answerList[1] + "</label>"
+        + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[3].answerList[2] + "</label>"
+        + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[3].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 5
+    $("#question5").html("<h3>" + questions[4].question + "</h3>");
+    $("#answer5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[4].answerList[0] + "</label>"
+        + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[4].answerList[1] + "</label>"
+        + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[4].answerList[2] + "</label>"
+        + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[4].answerList[3] + "</label><br><br>"
+    );
+    // Question 6
+    $("#question6").html("<h3>" + questions[5].question + "</h3>");
+    $("#answer6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[5].answerList[0] + "</label>"
+        + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[5].answerList[1] + "</label>"
+        + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[5].answerList[2] + "</label>"
+        + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[5].answerList[3] + "</label><br><br>"
+    );
+    // Question 7
+    $("#question7").html("<h3>" + questions[6].question + "</h3>");
+    $("#answer7").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[6].answerList[0] + "</label>"
+        + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[6].answerList[1] + "</label>"
+        + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[6].answerList[2] + "</label>"
+        + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[6].answerList[3] + "</label><br><br>"
+    );
+
+    // Submit button
+    $("#submit").html("<button id='done' class='btn btn-success'>RESULTS</button>");
+
+    // Click event runs keepingScore() and displayResults() when user clicks Done button
+    $("#done").on("click", function() {
+
+        // Keeping track of score based on correct, incorrect, and unanswered
+        keepingScore();
+
+        // Display 
+        displayResults();
+        
     });
+});
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+// Timer countdown function
+function run() {
 
-  function showResults() {
-    // gather answer containers from our quiz
-    var answerContainers = quizContainer.querySelectorAll(".answers");
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
 
-    // keep track of user's answers
-    let numCorrect = 0;
-    let numWrong = 0;
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      var answerContainer = answerContainers[questionNumber];
-      var selector = `input[name=question${questionNumber}]:checked`;
-      var userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
+function decrement() {
 
-      } else{
-        numWrong++;
-      }
-    });
+    //  Decrease number by one.
+    number--;
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} correct answers!`;
-    wrongsContainer.innerHTML = `${numWrong} of wrong options!`;
-  }
+    //  Show the number in the #time tag
+    $("#time").html("<h2>Time Remaining: " + number + " Seconds</h2>" + "<br>");
 
-  var quizContainer = document.getElementById("trivia");
-  var resultsContainer = document.getElementById("results");
-  var wrongsContainer = document.getElementById("wrongs");
-  var submitButton = document.getElementById("submit");
-  var myQuestions = [
-    {
-      question: "Who is the strongest?",
-      answers: {
-        a: "Superman",
-        b: "The Terminator",
-        c: "Waluigi, obviously"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "What is the best site ever created?",
-      answers: {
-        a: "SitePoint",
-        b: "Simple Steps Code",
-        c: "Trick question; they're both the best"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Where is Waldo really?",
-      answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-      },
-      correctAnswer: "c"
+    if (number === 0) {
+
+        // Run stop function to stop timer countdown
+        stop();
+
+        keepingScore();
+        displayResults();
+
     }
-  ];
+}
 
-  // display quiz right away
-  showTrivia();
+function stop() {
 
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
-})();
+    //  Clears intervalId
+    clearInterval(intervalId);
+}
+
+// Function used for displaying results in terms of correct, incorrect, and unanswered --- I want to put all of these tags inside a div so that i can just hide the parent div
+function displayResults() {
+
+    $("#time").hide();
+    $("#question1").hide();
+    $("#answer1").hide();
+    $("#question2").hide();
+    $("#answer2").hide();
+    $("#question3").hide();
+    $("#answer3").hide();
+    $("#question4").hide();
+    $("#answer4").hide();
+    $("#question5").hide();
+    $("#answer5").hide();
+    $("#question6").hide();
+    $("#answer6").hide();
+    $("#question7").hide();
+    $("#answer7").hide();
+    $("#submit").hide();
+
+    $("#resultsMessage").html("<h3>All Done!</h3>");
+    $("#correct").html("Correct Answers: " + correctAnswers);
+    $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
+    $("#unanswered").html("Unanswered: " + unanswered);
+}
+
+// Function keeps score in terms of correct, incorrect, and unanswered --- I still want to make this a reusable piece so that I don't have to repeat it for each question
+function keepingScore() {
+
+    var userAnswer1 = $("input[name='answer1']:checked").val();
+    var userAnswer2 = $("input[name='answer2']:checked").val();
+    var userAnswer3 = $("input[name='answer3']:checked").val();
+    var userAnswer4 = $("input[name='answer4']:checked").val();
+    var userAnswer5 = $("input[name='answer5']:checked").val();
+    var userAnswer6 = $("input[name='answer6']:checked").val();
+    var userAnswer7 = $("input[name='answer7']:checked").val();
+
+    // Question 1
+    if (userAnswer1 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer1 == questions[0].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 2
+    if (userAnswer2 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer2 == questions[1].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 3
+    if (userAnswer3 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer3 == questions[2].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 4
+    if (userAnswer4 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer4 == questions[3].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 5
+    if (userAnswer5 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer5 == questions[4].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 6
+    if (userAnswer6 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer6 == questions[5].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 7
+    if (userAnswer7 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer7 == questions[6].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }  
+}
